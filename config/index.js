c9ee72301896d355c7818af9b7ecfa8c84190866
M.Ghiasi <emg110@gmail.com>
2019-03-15 15:03:48 +0330

    - Removing config index.js to add it again fresh.

--- config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    USER_SELF: "houmanproject",
-    ENVIRONMENT: process.env.NODE_ENV || "development",
-    PORT: process.env.PORT || "3003",
-    HOST: process.env.HOST || "localhost",
-    CSRFTOKEN : "UBA6niBPuyBJFExgL4SxbrplJ2YEhuTR",
-    SESSIONID : "10817952003%3ASjIBjFWFEnwRwC%3A7",
-    LOGINMODE: "NONE" // This can be either CLI, API or NONE
-};
