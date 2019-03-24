80a30af230694e0b373756cedd063d30f01cf08a
M.Ghiasi <emg110@gmail.com>
2019-03-24 19:50:15 +0430

    -Adding ETL server config and start to main index of application beside Feathers API server

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const apiServer = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+apiServer.on('listening', () =>
+  logger.info('API Server started on http://%s:%d', app.get('host'), port)
 );
+
+
+const { HOST, PORT } = require("../config");
+const application = require("../etl/app");
+const instagram = require('../etl/api/instagram/index').instagramClient;
+console.log('**************************************');
+const etlServer = application.listen(PORT, () => {
+  //console.log(`ETL Server Listening on ${HOST}:${PORT}`)
+  logger.info('ETL Server Listening on http://%s:%d', HOST, PORT)
+});
