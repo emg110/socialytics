675859cc1b7b4385f97294c31b4b8cab8a671169
M.Ghiasi <emg110@gmail.com>
2019-04-23 21:43:38 +0430

    - Remembering hits in between session refreshed via local storage

--- public/js/client.js
+++ public/js/client.js
@@ -29,6 +29,16 @@ Holder.addTheme("white", {
   fg: "#a7a7a7",
   size: 10
 });
+var ctrs = ls.getItem('counters');
+ctrs = JSON.parse(ctrs);
+for(i in Object.keys(ctrs)){
+  var key = Object.keys(ctrs)[i];
+  var value = ctrs[Object.keys(ctrs)[i]];
+  if(key !== "empty"){
+    var querySelector = "[value="+"'"+key+"'"+"]"
+    $(querySelector).append('<span data-toggle="popover" data-trigger="hover" title="Last updated" data-content="' + 'Page refreshed!' + '" style="background-color:white; color:#007bff; border-radius:50%;margin-left:10px; width:25px;height:25px;vertical-align: middle " class="badge pulse-default" >' + value + '</span>');
+  }
+}
 function ig_media_preview(base64data) {
   var jpegtpl = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEABsaGikdKUEmJkFCLy8vQkc/Pj4/R0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0cBHSkpNCY0PygoP0c/NT9HR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR//AABEIABQAKgMBIgACEQEDEQH/xAGiAAABBQEBAQEBAQAAAAAAAAAAAQIDBAUGBwgJCgsQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+gEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoLEQACAQIEBAMEBwUEBAABAncAAQIDEQQFITEGEkFRB2FxEyIygQgUQpGhscEJIzNS8BVictEKFiQ04SXxFxgZGiYnKCkqNTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqCg4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2dri4+Tl5ufo6ery8/T19vf4+fr/2gAMAwEAAhEDEQA/AA==",
     t = atob(base64data),
@@ -77,7 +87,170 @@ function backNav() {
   /*$("#instagram-container").show();*/
 
 }
+function render(html, container){
+  document.getElementById('social-sidepanel').style.backgroundColor = "#fff";
+  OverlayScrollbars(document.getElementById('social-sidepanel'), {
+    className : "os-theme-dark",
+  });
+  var index = parseInt(ls.getItem('currentFreeIndex'));
+  var results = JSON.parse(ls.getItem('results'));
+  var counters = JSON.parse(ls.getItem('counters'));
+  results[index] = {html: html};
 
+  document.getElementById('instagram-container').innerHTML = html;
+  if (container !== 'main') {
+    $("#backbtn").show();
+  }
+
+  index++;
+  var currentVal = $('.disabled').val();
+  /*if (currentVal) {
+    currentVal = currentVal.replace('Loading... ', 'Get ');
+  }*/
+
+  $('.disabled').val(currentVal);
+  $('.disabled').css('background-color', '#007bff');
+  $('.disabled').find('#loading').remove();
+  var doneDate = dayjs(Date.now()).format('YYYY MM-DD HH:mm:ss')
+  //<span class="badge">7</span>
+  $('.disabled').next().remove();
+  $('.disabled').after('<span style="color:lawngreen;" class="glyphicon glyphicon-ok"></span>');
+  var count = 0;
+  if (counters[currentVal]) {
+    if (counters[currentVal] >= 1) {
+      count = counters[currentVal] + 1;
+      counters[currentVal]++;
+    } else {
+      counters[currentVal] = 1;
+      count = 1
+    }
+  } else {
+    counters[currentVal] = 1;
+    count = 1
+  }
+  if (count > 0) {
+    $('.disabled').find('span').remove()
+    $('.disabled').append('<span data-toggle="popover" data-trigger="hover" title="Last updated" data-content="' + doneDate + '" style="background-color:white; color:#007bff; border-radius:50%;margin-left:10px; width:25px;height:25px;vertical-align: middle " class="badge pulse-default" >' + count + '</span>');
+  }
+  var elem = $('.disabled').find('span');
+  $(elem).popover();
+  $('.disabled').removeClass('disabled');
+  var optionsEngagement = {
+    width:'150px',
+    height:'40px',
+    type: 'line',
+    lineColor: '#e0c526',
+    fillColor: '#e08e0b',
+    lineWidth: 3,
+    highlightLineColor: '#ffffff'
+  };
+  var optionsLikes = {
+    width:'150px',
+    height:'40px',
+    type: 'line',
+    lineColor: '#44ff6e',
+    fillColor: '#21c936',
+    lineWidth: 3,
+    highlightLineColor: '#ffffff'
+  };
+  var optionsComments = {
+    width:'150px',
+    height:'40px',
+    type: 'line',
+    lineColor: '#003f7f',
+    fillColor: '#5690c9',
+    lineWidth: 3,
+    highlightLineColor: '#ffffff'
+  };
+  var optionsSentiments = {
+    width:'150px',
+    height:'40px',
+    type: 'line',
+    lineColor: '#7f2e29',
+    fillColor: '#c9424a',
+    lineWidth: 3,
+    highlightLineColor: '#ffffff'
+  };
+  engagementData =  $('#sparkline-data').attr('sparkline-data');
+  likesData =  $('#sparkline-likes-data').attr('sparkline-data');
+  commentsData =  $('#sparkline-comments-data').attr('sparkline-data');
+  sentimentData =  $('#sparkline-likes-data').attr('sparkline-data');
+  if(engagementData){
+    engagementData = engagementData.split(',') || [];
+    if(engagementData.length>=1){
+      $('#sparkline').sparkline(engagementData, optionsEngagement)
+    }
+  }
+  if(likesData){
+    likesData = likesData.split(',') || [];
+    if(likesData.length>=1){
+      $('#sparkline-likes').sparkline(likesData, optionsLikes)
+    }
+  }
+  if(commentsData){
+    commentsData = commentsData.split(',') || [];
+    if(commentsData.length>=1){
+      $('#sparkline-comments').sparkline(commentsData, optionsComments)
+    }
+  }
+  if(sentimentData){
+    sentimentData = sentimentData.split(',') || [];
+    if(sentimentData.length>=1){
+      $('#sparkline-sentiments').sparkline(sentimentData, optionsSentiments)
+    }
+  }
+  ls.setItem('results',JSON.stringify(results));
+  ls.setItem('currentFreeIndex',index.toString());
+  ls.setItem('counters',JSON.stringify(counters));
+  /*  var myChart = echarts.init(document.getElementById('sparkline'));
+
+    // specify chart configuration item and data
+    var option = {
+      title: {
+        text: 'Engagement activity'
+      },
+      tooltip: {},
+      legend: {
+        data:['Engagement']
+      },
+      xAxis: {
+        data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+      },
+      yAxis: {},
+      series: [{
+        name: 'Engagement',
+        type: 'bar',
+        data: engagementData
+      }]
+    };
+
+    // use configuration item and data specified to show chart
+    myChart.setOption(option);*/
+}
+function norender(){
+  closeNav('social-sidepanel');
+  console.log('Did not provide all required input data');
+  window.alert('please provide all required');
+  //var currentVal = $('.disabled').val();
+  //if (currentVal) currentVal = currentVal.replace('Loading... ', 'Get ');
+  //$('.disabled').val(currentVal);
+  $('#loading').remove();
+  $('.disabled').css('background-color', '#007bff');
+  //$('.disabled').next().remove();
+  $('.disabled').removeClass('fired');
+  $('.btn-primary').removeClass('fired');
+  $('.btn-primary').removeClass('disabled');
+}
+function renderText(txt){
+  var json = JSON.parse(txt);
+  document.getElementById('username-input').value = json.username;
+  document.getElementById('tag-input').value = json.tag;
+  document.getElementById('location-input').value = json.location;
+  document.getElementById('search-input').value = json.query;
+  document.getElementById('count-input').value = json.count;
+  document.getElementById('shortcode-input').value = json.shortcode;
+  console.log(json);
+}
 function getEndpoint(endpoint, type, code, container) {
   $("#carosel").hide();
   $("#logo").show();
@@ -252,9 +425,6 @@ function getEndpoint(endpoint, type, code, container) {
       ls.setItem('currentFreeIndex','0');
       $("#backbtn").hide();
     }
-    /*else {
-      $("#backbtn").show();
-    }*/
     if (url !== "NA" && url.indexOf('http://') >= 0) {
       $('.fired').addClass('disabled');
       $('.disabled').removeClass('fired');
@@ -265,160 +435,12 @@ function getEndpoint(endpoint, type, code, container) {
       xhr.open("GET", url, true);
       xhr.onload = function (e) {
         var html = xhr.responseText;
-        document.getElementById('social-sidepanel').style.backgroundColor = "#fff";
-        OverlayScrollbars(document.getElementById('social-sidepanel'), {
-          className : "os-theme-dark",
-        });
-        var index = parseInt(ls.getItem('currentFreeIndex'));
-        var results = JSON.parse(ls.getItem('results'));
-        var counters = JSON.parse(ls.getItem('counters'));
-        results[index] = {html: html};
-
-        document.getElementById('instagram-container').innerHTML = html;
-        if (container !== 'main') {
-          $("#backbtn").show();
-        }
-
-        index++;
-        var currentVal = $('.disabled').val();
-        if (currentVal) {
-          currentVal = currentVal.replace('Loading... ', 'Get ');
-        }
-
-        $('.disabled').val(currentVal);
-        $('.disabled').css('background-color', '#007bff');
-        $('.disabled').find('#loading').remove();
-        var doneDate = dayjs(Date.now()).format('YYYY MM-DD HH:mm:ss')
-        //<span class="badge">7</span>
-        $('.disabled').next().remove();
-        $('.disabled').after('<span style="color:lawngreen;" class="glyphicon glyphicon-ok"></span>');
-        var count = 0;
-        if (counters[currentVal]) {
-          if (counters[currentVal] >= 1) {
-            count = counters[currentVal] + 1;
-            counters[currentVal]++;
-          } else {
-            counters[currentVal] = 1;
-            count = 1
-          }
-        } else {
-          counters[currentVal] = 1;
-          count = 1
-        }
-        if (count > 0) $('.disabled').append('<span data-toggle="popover" data-trigger="hover" title="Last updated" data-content="' + doneDate + '" style="background-color:white; color:#007bff; border-radius:50%;margin-left:10px " class="badge pulse-default" >' + count + '</span>');
-        var elem = $('.disabled').find('span');
-        $(elem).popover();
-        $('.disabled').removeClass('disabled');
-        var optionsEngagement = {
-          width:'150px',
-          height:'40px',
-          type: 'line',
-          lineColor: '#e0c526',
-          fillColor: '#e08e0b',
-          lineWidth: 3,
-          highlightLineColor: '#ffffff'
-        };
-        var optionsLikes = {
-          width:'150px',
-          height:'40px',
-          type: 'line',
-          lineColor: '#44ff6e',
-          fillColor: '#21c936',
-          lineWidth: 3,
-          highlightLineColor: '#ffffff'
-        };
-        var optionsComments = {
-          width:'150px',
-          height:'40px',
-          type: 'line',
-          lineColor: '#003f7f',
-          fillColor: '#5690c9',
-          lineWidth: 3,
-          highlightLineColor: '#ffffff'
-        };
-        var optionsSentiments = {
-          width:'150px',
-          height:'40px',
-          type: 'line',
-          lineColor: '#7f2e29',
-          fillColor: '#c9424a',
-          lineWidth: 3,
-          highlightLineColor: '#ffffff'
-        };
-        engagementData =  $('#sparkline-data').attr('sparkline-data');
-        likesData =  $('#sparkline-likes-data').attr('sparkline-data');
-        commentsData =  $('#sparkline-comments-data').attr('sparkline-data');
-        sentimentData =  $('#sparkline-likes-data').attr('sparkline-data');
-        if(engagementData){
-          engagementData = engagementData.split(',') || [];
-          if(engagementData.length>=1){
-            $('#sparkline').sparkline(engagementData, optionsEngagement)
-          }
-        }
-        if(likesData){
-          likesData = likesData.split(',') || [];
-          if(likesData.length>=1){
-            $('#sparkline-likes').sparkline(likesData, optionsLikes)
-          }
-        }
-        if(commentsData){
-          commentsData = commentsData.split(',') || [];
-          if(commentsData.length>=1){
-            $('#sparkline-comments').sparkline(commentsData, optionsComments)
-          }
-        }
-        if(sentimentData){
-          sentimentData = sentimentData.split(',') || [];
-          if(sentimentData.length>=1){
-            $('#sparkline-sentiments').sparkline(sentimentData, optionsSentiments)
-          }
-        }
-        ls.setItem('results',JSON.stringify(results));
-        ls.setItem('currentFreeIndex',index.toString());
-        ls.setItem('counters',JSON.stringify(counters));
-      /*  var myChart = echarts.init(document.getElementById('sparkline'));
-
-        // specify chart configuration item and data
-        var option = {
-          title: {
-            text: 'Engagement activity'
-          },
-          tooltip: {},
-          legend: {
-            data:['Engagement']
-          },
-          xAxis: {
-            data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
-          },
-          yAxis: {},
-          series: [{
-            name: 'Engagement',
-            type: 'bar',
-            data: engagementData
-          }]
-        };
-
-        // use configuration item and data specified to show chart
-        myChart.setOption(option);*/
-
+       render(html,container);
       }
       xhr.send();
     }
     else {
-      closeNav('social-sidepanel');
-      console.log('please provide all required');
-      window.alert('please provide all required');
-      var currentVal = $('.disabled').val();
-      if (currentVal) currentVal = currentVal.replace('Loading... ', 'Get ');
-      $('.disabled').val(currentVal);
-      $('#loading').remove();
-      $('.disabled').css('background-color', '#007bff');
-      $('.disabled').next().remove();
-      $('.disabled').removeClass('fired');
-      $('.btn-primary').removeClass('fired');
-      $('.btn-primary').removeClass('disabled');
-
-
+      norender();
     }
   }
   else {
@@ -426,14 +448,7 @@ function getEndpoint(endpoint, type, code, container) {
     xhr.open("GET", endpoint, true);
     xhr.onload = function (e) {
       var txt = xhr.responseText;
-      var json = JSON.parse(txt);
-      document.getElementById('username-input').value = json.username;
-      document.getElementById('tag-input').value = json.tag;
-      document.getElementById('location-input').value = json.location;
-      document.getElementById('search-input').value = json.query;
-      document.getElementById('count-input').value = json.count;
-      document.getElementById('shortcode-input').value = json.shortcode;
-      console.log(json);
+      renderText(txt);
     }
     xhr.send();
 
@@ -480,10 +495,10 @@ $('.btn-primary').on('click', function () {
   var endpoint = $(this).attr('click-target');
   var type = $(this).attr('call-type');
   if(type !== 'form-data') {
-    var currentVal = $(this).val().replace('Get ', 'Loading... ');
-    $(this).val(currentVal);
+    //var currentVal = $(this).val().replace('Get ', 'Loading... ');
+    //$(this).val(currentVal);
     $(this).addClass('fired');
-    $(this).find('span').remove();
+    //$(this).find('span').remove();
   }
   getEndpoint(endpoint, type, '', 'main');
 });
