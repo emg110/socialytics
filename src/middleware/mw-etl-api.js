70f295ada37f05cba4c96b5f29fdc4f09e0e758a
M.Ghiasi <emg110@gmail.com>
2019-05-21 22:31:56 +0430

    - Added links and text content

--- src/middleware/mw-etl-api.js
+++ src/middleware/mw-etl-api.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch');
 //const ejs = require('ejs')
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36';
 const uri = config.PROTOCOL+"://"+config.HOST+':'+config.UIPORT+'/'
+
 //console.log(uri);
 const writeDatabase = require('../storage');
 const renderData = require('../render');
