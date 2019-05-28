15e4d8d86ac2d469a1b311ce9bd4d1844f869b2f
M.Ghiasi <emg110@gmail.com>
2019-05-28 16:06:52 +0430

    -Fixed minor issue in client.js & main.ejs

--- public/js/client.js
+++ public/js/client.js
@@ -37,13 +37,9 @@ $('.btn-primary').on('click', function () {
       var caller = this;
       if (type !== 'form-data') {
         $(this).addClass('fired');
-        getEndpointData(endpoint, type, '', 'main', caller);
-
-      }else{
-        getEndpointData('/instagram/sets/data?', 'form-data', '', 'main', undefined);
       }
-      // console.log(caller);
-
+      getEndpointData('/instagram/sets/data?', 'form-data', '', 'main', undefined);
+      getEndpointData(endpoint, type, '', 'main', caller);
     }
   }
 });
