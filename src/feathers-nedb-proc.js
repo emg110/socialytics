2ff59f6069bdf5753172d861799fd9de775048a8
M.Ghiasi <emg110@gmail.com>
2019-05-29 01:10:53 +0430

    -For now only recent 20 post of each profile will be processed due to lack of resources (tuning to be continued)

--- src/feathers-nedb-proc.js
+++ src/feathers-nedb-proc.js
@@ -94,6 +94,7 @@ module.exports = function () {
     if(context.method === 'find' && context.params.textProc){
       let resComments = {};
       let resCaptions = {};
+      context.result.data = context.result.data.slice(0,20);
       for (let item of context.result.data){
         if(context.params.textProc){
           if(item.comments.count > 0){
@@ -101,18 +102,37 @@ module.exports = function () {
             if(Array.isArray(item.comments.edges)){
               for(let comment of item.comments.edges){
                 knwlInstance.init(comment.text);
+                let links =Autolinker.parse(comment.text,{
+                  urls:true,
+                /*  email:true,
+                  phone:true,*/
+                  mention:'instagram',
+                  hashtag:'instagram',
+                  stripPrefix:false
+                })
+                for (let i in links){
+                  links[i] = {link:links[i].matchedText}
+
+                }
+                let emojsent = emojiSentiment(comment.text)
+                for (let j in emojsent){
+                  emojsent[j] = {emoji:emojsent[j].emoji,code:emojsent[j].code,sentiment:emojsent[j].sentiment}
+                }
+                let sentim = sentiment(comment.text)
+                sentim = {score:sentim.score,vote:sentim.vote}
                 resComments[item.shortcode].push({
                   created_at:comment.created_at,
-                  text:comment.text,
+                  /*text:comment.text,*/
                   owner:{
-                    pic_url:comment.owner.profile_pic_url,
+                    /*pic_url:comment.owner.profile_pic_url,*/
                     username: comment.owner.username,
                     gender:genderDetect.detect(comment.owner.username)
                   },
                   word_count:wordcount(comment.text),
-                  sentiment:sentiment(comment.text),
-                  emoji: emojiExtract.extractEmoji(comment.text),
-                  emojiSentiments:emojiSentiment(comment.text),
+                  count_words:countWords(comment.text,true),
+                  sentiment:sentim,
+                  emojis: emojiExtract.extractEmoji(comment.text),
+                  emojiSentiments:emojsent,
                   keywords: keywordExtractor.extract(comment.text,{}),
                   ner:{
                     dates: knwlInstance.get('dates'),
@@ -120,17 +140,9 @@ module.exports = function () {
                     phones:knwlInstance.get('phones'),
                     links:knwlInstance.get('links'),
                     emails:knwlInstance.get('emails'),
-                    places:knwlInstance.get('places')
-                  },
-                  count_words:countWords(comment.text,true),
-                  links: Autolinker.parse(comment.text,{
-                    urls:true,
-                    email:true,
-                    phone:true,
-                    mention:'instagram',
-                    hashtag:'instagram',
-                    stripPrefix:false
-                  })
+                    places:knwlInstance.get('places'),
+                    webs:links
+                  }
                 })
               }
             }
@@ -139,12 +151,32 @@ module.exports = function () {
             resCaptions[item.shortcode]=[];
             for(let caption of item.captions.edges){
               knwlInstance.init(caption.node.text);
+              let linksCap =Autolinker.parse(caption.node.text,{
+                urls:true,
+           /*     email:true,
+                phone:true,*/
+                mention:'instagram',
+                hashtag:'instagram',
+                stripPrefix:false
+              })
+              for (let i in linksCap){
+                linksCap[i] = {link:linksCap[i].matchedText}
+
+              }
+              let emojsentCap = emojiSentiment(caption.node.text)
+              for (let j in emojsentCap){
+                emojsentCap[j] = {emoji:emojsentCap[j].emoji,code:emojsentCap[j].code,sentiment:emojsentCap[j].sentiment}
+              }
+              let sentimCap = sentiment(caption.node.text)
+              sentimCap = {score:sentimCap.score,vote:sentimCap.vote}
               resCaptions[item.shortcode].push({
-                text:caption.node.text,
+                created_at:item.created_at,
+               /* text:caption.node.text,*/
                 word_count:wordcount(caption.node.text),
-                sentiment:sentiment(caption.node.text),
+                count_words:countWords(caption.node.text,true),
+                sentiment:sentimCap,
                 emoji: emojiExtract.extractEmoji(caption.node.text),
-                emojiSentiments:emojiSentiment(caption.node.text),
+                emojiSentiments:emojsentCap,
                 keywords: keywordExtractor.extract(caption.node.text,{}),
                 ner:{
                   dates: knwlInstance.get('dates'),
@@ -152,18 +184,9 @@ module.exports = function () {
                   phones:knwlInstance.get('phones'),
                   links:knwlInstance.get('links'),
                   emails:knwlInstance.get('emails'),
-                  places:knwlInstance.get('places')
-                },
-                count_words:countWords(caption.node.text,true),
-                links: Autolinker.parse(caption.node.text,{
-                  urls:true,
-                  email:true,
-                  phone:true,
-                  mention:'instagram',
-                  hashtag:'instagram',
-                  stripPrefix:false
-                })
-
+                  places:knwlInstance.get('places'),
+                  webs: linksCap
+                }
               })
             }
           }
